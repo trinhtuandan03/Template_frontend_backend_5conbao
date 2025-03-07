f6338edd730a85d9c6add45d82a11c3e77b6f8d6
trinhtuandan03 <trinhtuandan1601@gmail.com>
2025-03-07 14:16:39 +0700

    fontend admin

--- frontend/src/controllers/admin/admincontroller.js
+++ frontend/src/controllers/admin/admincontroller.js
@@ -2,24 +2,12 @@ var express = require("express");
 var router = express.Router();
 var User = require("../../model/user");
 
+router.get("/HomeManage/Index", function(req, res) {
+    res.render("admin/HomeManage/Index.ejs");
+});
 
-router.get("/UserManage", function(req, res) {
-    res.render("admin/UserManage/userManage.ejs");
-});
-router.get("/SanPhamManage/Create", function(req, res) {
-    res.render("admin/SanPhamManage/Create.ejs");
-});
-router.get("/SanPhamManage/Delete", function(req, res) {
-    res.render("admin/SanPhamManage/Delete.ejs");
-});
-router.get("/SanPhamManage/Details", function(req, res) {
-    res.render("admin/SanPhamManage/Details.ejs");
-});
-router.get("/SanPhamManage/Edit", function(req, res) {
-    res.render("admin/SanPhamManage/Edit.ejs");
-});
-router.get("/SanPhamManage/Index", function(req, res) {
-    res.render("admin/SanPhamManage/Index.ejs");
+router.get("/HomeManage/widgets", function(req, res) {
+    res.render("admin/HomeManage/widgets.ejs");
 });
 
 
